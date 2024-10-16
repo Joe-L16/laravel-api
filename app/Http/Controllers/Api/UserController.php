4a3fddbb27f6dca15ba3a086905e2f4309456bcf
Joe L <munir.sirojoel16@gmail.com>
2024-10-16 09:31:07 +0700

    perf: :zap: "Added index method to UserController, returning a JSON response with a 'User List' message."

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(['message' => 'User List']);
+    }
 }
