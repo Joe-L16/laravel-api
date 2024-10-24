fafa1f26aa85476ba2ef1e2190210894694e1918
Joe L <munir.sirojoel16@gmail.com>
2024-10-24 10:36:11 +0700

    feat: :sparkles:

---
+++ database/migrations/2024_10_24_032310_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('author_id');
+            $table->integer('published_year');
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
