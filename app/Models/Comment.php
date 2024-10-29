48b6f34f8a16c800c7206587a761c1041bf339e2
Joe L <munir.sirojoel16@gmail.com>
2024-10-29 20:52:45 +0700

    feat: :sparkles:

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Post extends Model
+class Comment extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'content'];
+    protected $fillable = ['comment', 'post_id'];
 
-    public function comments()
-    {
-        return $this->hasMany(Comment::class);
+    public function post() {
+        return $this->belongsTo(Post::class);
     }
 }
+
