435616ee7cd20a7ffbfbdb5c3a10c5092c8e8abe
Joe L <munir.sirojoel16@gmail.com>
2024-10-29 21:45:40 +0700

    "Added show method to Controller, updated use statements"

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Models\Post;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
+    public function show($id) {
+        $post = Post::with('comments')->find($id);
+        return response()->json($post);
+    }
+
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
