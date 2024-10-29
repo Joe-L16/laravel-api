93852a83b7228177a27a3c6b7f219b0c78940417
Joe L <munir.sirojoel16@gmail.com>
2024-10-29 22:50:58 +0700

    "Swapped use statements and added 'name' and 'birthdate' columns to authors table"

--- database/migrations/2024_10_29_154750_create_authors_table.php
+++ database/migrations/2024_10_29_154750_create_authors_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateAuthorsTable extends Migration
 {
@@ -15,6 +15,8 @@ class CreateAuthorsTable extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->date('birthdate');
             $table->timestamps();
         });
     }
